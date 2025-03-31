6a13161e6e0a3b56c8c4250c38e94482e1dbe244
Michael Munga <micknyalle@gmail.com>
2025-04-01 00:17:27 +0300

    add index.js and package-lock.json

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+  console.log(`I'm ${age} years old. Happy birthday to me!`);
+  debugger;
+}
+function writeCards(names, event) {
+  return names.map(
+    (name) => `Thank you, ${name}, for the wonderful ${event} gift!`
+  );
+}
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+}
+
+countDown(10);
